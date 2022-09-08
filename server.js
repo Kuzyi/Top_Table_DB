f63c4688abff23d0b3e4be3406d0de38f88a7c76
kuzyi <aleckuzma@gmail.com>
2022-09-08 18:44:22 -0400

    changed password to passwordDigest in user model

--- server.js
+++ server.js
@@ -1,18 +1,19 @@
 const express = require('express')
 const cors = require('cors')
 const logger = require('morgan')
+const AppRouter = require('./routes/AppRouter')
+
 const PORT = process.env.PORT || 3001
 
 const app = express()
-const AppRouter = require('./routes/AppRouter')
 
 app.use(cors())
 app.use(logger('dev'))
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
-app.get('/', (req, res) => res.json({ message: 'Server Works' }))
-app.use('/api', AppRouter)
 
+app.use('/api', AppRouter)
+app.get('/', (req, res) => res.json({ message: 'Server Works' }))
 app.listen(PORT, () => {
   console.log(`App listening on port: ${PORT}`)
 })
